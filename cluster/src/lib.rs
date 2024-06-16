3f8c31ed80c72fae1cebc31da0ce607c134c9fe5
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-06-16 17:22:36 +0800

    Remove unused feature

--- cluster/src/lib.rs
+++ cluster/src/lib.rs
@@ -1,10 +1,3 @@
-#![feature(new_uninit)]
-#![feature(coroutines)]
-#![feature(iter_from_coroutine)]
-#![feature(type_alias_impl_trait)]
-#![feature(test)]
-#![feature(vec_push_within_capacity)]
-
 #![allow(clippy::needless_range_loop)]
 #![allow(clippy::map_flatten)]
 #![allow(clippy::manual_memcpy)]
