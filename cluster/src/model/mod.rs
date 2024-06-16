0f492da34455d5aa9f486635f59329a46c6b85fc
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-06-16 16:03:35 +0800

    Remove ClusterModel enum

--- cluster/src/model/mod.rs
+++ cluster/src/model/mod.rs
@@ -1,12 +1,5 @@
 //! Implementation of various clustering algorithms.
 
-#[derive(Debug, Clone, Copy)]
-pub enum ClusterModel {
-    KMeans,
-    GaussianMixture,
-    AgglomerativeSingleLinkage,
-}
-
 pub mod init;
 pub mod k_means;
 pub mod gaussian_mixture;
