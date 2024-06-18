a880a9bd44f0fbc11b1b79f96608e73f0eb1cb3d
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-06-19 02:53:24 +0800

    Clean up SLINK implementation

--- cluster/src/slink.rs
+++ cluster/src/slink.rs
@@ -8,12 +8,13 @@ pub fn slink<T, D>(samples : &[T], mut dissimilarity: D) -> Dendrogram
 where
     D: FnMut(&T, &T) -> f64
 {
-    let mut merge_heights = vec![0.0f64; samples.len()];
-    let mut merge_targets = vec![0usize; samples.len()];
-    let mut merge_updates = vec![0.0f64; samples.len()];
+    let mut merge_heights = vec![0.0; samples.len()];
+    let mut merge_targets = vec![0;   samples.len()];
+    let mut merge_updates = vec![0.0; samples.len()];
     for n in 0..samples.len() {
-        // 1: Compute merge_updates - an estimation on the lowest level at which i merge to the
-        //    right with n.
+        // Compute merge_updates - the lowest level at which i merge to the right with n. Note that
+        // our computation is not the same as in original paper, in the sense that our
+        // merge_updates is not the same as the mu function found in the original paper.
         {
             // By definition, i merge with n directly when height is equal to their dissimilarity
             // if they have not already been merged.
@@ -21,18 +22,16 @@ where
                 merge_updates[i] = dissimilarity(&samples[i], &samples[n]);
             }
 
-            // 1: We merge to the right with merge_targets[i] at merge_heights[i].
-            // 2: We merge to the right with n                at merge_updates[i].
-            // Therefore, merge_targets[i] must have merge to the right with n at max(merge_heights[i], merge_updates[i]).
-            for i in 0..n {
-                merge_updates[merge_targets[i]] = merge_updates[merge_targets[i]].min(f64::max(
-                    merge_heights[i],
-                    merge_updates[i],
-                ));
-            }
+            // Note we have i merge with merge_targets[i] at merge_heights[i] without considering n.
+            // Hence, we have the following bounds
+            //  - merge_updates[i]                <= max(merge_heights[i], merge_updates[merge_targets[i]])
+            //  - merge_updates[merge_targets[i]] <= max(merge_heights[i], merge_updates[i])
+            // We have to be kinda careful about the order of update.
+            for i in 0..n { merge_updates[merge_targets[i]] = merge_updates[merge_targets[i]].min(f64::max(merge_heights[i], merge_updates[i]               )); }
+            for i in 0..n { merge_updates[i]                = merge_updates[i]               .min(f64::max(merge_heights[i], merge_updates[merge_targets[i]])); }
         }
 
-        // 2: Update merge_heights and merge_targets.
+        // Update merge_heights and merge_targets.
         {
             // Initialization
             merge_heights[n] = f64::INFINITY;
@@ -42,15 +41,6 @@ where
             for i in 0..n {
                 if merge_heights[i] >= merge_updates[i] {
                     merge_heights[i] = merge_updates[i];
-                    merge_targets[i] = n;
-                }
-            }
-
-            // We have merge_targets[i] merge to something before i merge to merge_targets[i]. The
-            // only possibility of that something is n. When i merge to merge_targets[i], the
-            // rightmost sample is no longer merge_targets[i] but n instead.
-            for i in 0..n {
-                if merge_heights[i] >= merge_heights[merge_targets[i]] {
                     merge_targets[i] = n;
                 }
             }
