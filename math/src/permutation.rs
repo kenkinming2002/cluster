bd851dc6b010e1d9d6bcfea18781a2744faa556a
Kwok Kin Ming <kenkinming2002@gmail.com>
2025-05-14 15:38:41 +0800

    Fix compilation error
    Fix compilation error by moving the defining use of the opaque type
    PermutationImpl into separate module.

--- math/src/permutation.rs
+++ math/src/permutation.rs
@@ -13,7 +13,33 @@ impl Parity {
     }
 }
 
-pub struct Permutations<const N: usize>(PermutationsImpl<N>);
+mod wrap {
+    use super::Parity;
+
+    pub type PermutationsImpl<const N: usize> = impl Iterator<Item = (Parity, [usize; N])>;
+
+    pub fn permutations_impl<const N: usize>() -> PermutationsImpl<N> {
+        std::iter::from_coroutine(#[coroutine] || {
+            let mut parity = Parity::Even;
+            let mut item = std::array::from_fn(|i| i);
+            loop {
+                yield (parity, item);
+
+                let Some(i) = (0..N-1).rev().find(|&i| item[i] < item[i+1]) else { break };
+                let Some(j) = (i+1..N).rev().find(|&j| item[i] < item[j])   else { unreachable!() };
+
+                item.swap(i, j);
+                item[i+1..].reverse();
+                if ((N - (i+1)) / 2 + 1) % 2 == 1 {
+                    parity = parity.flip();
+                }
+            }
+        })
+    }
+
+}
+
+pub struct Permutations<const N: usize>(wrap::PermutationsImpl<N>);
 
 impl<const N: usize> Default for Permutations<N> {
     fn default() -> Self {
@@ -23,7 +49,7 @@ impl<const N: usize> Default for Permutations<N> {
 
 impl<const N: usize> Permutations<N> {
     pub fn new() -> Self {
-        Self(permutations_impl())
+        Self(wrap::permutations_impl())
     }
 }
 
@@ -32,27 +58,6 @@ impl<const N: usize> Iterator for Permutations<N> {
     fn next(&mut self) -> Option<Self::Item> {
         self.0.next()
     }
-}
-
-type PermutationsImpl<const N: usize> = impl Iterator<Item = (Parity, [usize; N])>;
-
-fn permutations_impl<const N: usize>() -> PermutationsImpl<N> {
-    std::iter::from_coroutine(#[coroutine] || {
-        let mut parity = Parity::Even;
-        let mut item = std::array::from_fn(|i| i);
-        loop {
-            yield (parity, item);
-
-            let Some(i) = (0..N-1).rev().find(|&i| item[i] < item[i+1]) else { break };
-            let Some(j) = (i+1..N).rev().find(|&j| item[i] < item[j])   else { unreachable!() };
-
-            item.swap(i, j);
-            item[i+1..].reverse();
-            if ((N - (i+1)) / 2 + 1) % 2 == 1 {
-                parity = parity.flip();
-            }
-        }
-    })
 }
 
 #[cfg(test)]
